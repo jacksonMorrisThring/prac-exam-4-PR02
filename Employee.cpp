08f955f1f14199667de235687783aedd1fea791f
Jackson Morris-Thring <jacksonmorris-thring@student-10-201-32-017.wireless.adelaide.edu.au>
2022-10-26 13:25:48 +1030

    correction

--- Employee.cpp
+++ Employee.cpp
@@ -3,19 +3,20 @@
 
 Employee::Employee(){
     payRate = 0;
-    energyLevel = 1;
+    energyLevel = 100;
 }
 
 Employee::Employee(int payRate){
     this -> payRate = payRate;
+    energyLevel = 100;
 }  
 
 void Employee::takeABreak(int mins){
-    if(energyLevel + ((mins*2)/100) < 1){
-        energyLevel = energyLevel + mins/100;
+    if(energyLevel + mins*2 < 100){
+        energyLevel = energyLevel + mins*2;
     }
     else{
-        energyLevel = 1;
+        energyLevel = 100;
     }
 }
 
@@ -30,7 +31,7 @@ void Employee::set_payRate(int new_pay_rate){
     payRate = new_pay_rate;
 }
 void Employee::set_energyLevel(float newEnergyLevel){
-    if (newEnergyLevel <= 1 && newEnergyLevel >= 0)
+    if (newEnergyLevel <= 100 && newEnergyLevel >= 0)
     {
         energyLevel = newEnergyLevel;
     }
