1b68260c4796bc06fa14db4e6105f36dcf4d5981
Jackson Morris-Thring <jacksonmorris-thring@student-10-201-32-017.wireless.adelaide.edu.au>
2022-10-26 14:55:50 +1030

    grngrnb

--- Shop.cpp
+++ Shop.cpp
@@ -1,7 +1,10 @@
 #include "Shop.h"
+#include "Employee.h"
+#include "Casual.h"
 
-Shop(){
-    
+Shop::Shop(){
+    count = 0;
+    employees = new Employee*[5];
 }                          
 /* Creates a Shop containing pointers to 5 Employee objects as an array; 
    These Employee objects could be either Managers or Casuals, and in any order
@@ -9,12 +12,24 @@ Shop(){
 // Employee** employees
 // int count
 
-void addEmployee(Employee *employee){
+void Shop::addEmployee(Employee *employee){
+    employees[count] = employee;
+    count++;
     
 }   // Adds a employee to the Shop
-Employee **get_employees(){
+Employee **Shop::get_employees(){
+    return employees;
     
 }             // Returns the array of pointers to the five employee objects
-int get_count(){
+int Shop::get_count(){
+    return count;
+}      
+
+Shop::~Shop(){
+    for (int i = 0; i < count; i++)
+    {
+        delete employees[i];
+    }
+    delete[] employees;
     
-}                        
+}
--- Shop.h
+++ Shop.h
@@ -2,6 +2,7 @@
 #define SHOP_H
 
 #include "Employee.h"
+#include "Casual.h"
 
 
 class Shop{
@@ -16,7 +17,9 @@ public:
 
     void addEmployee(Employee *employee);   // Adds a employee to the Shop
     Employee **get_employees();             // Returns the array of pointers to the five employee objects
-    int get_count();                     
+    int get_count();       
+
+    ~Shop();              
 };
 
 #endif
